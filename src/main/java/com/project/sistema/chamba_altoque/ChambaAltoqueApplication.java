2cf656603dc322bd21058424c422a82d7d11c6ab
Diego-Lucar <lucardiego@gmail.com>
2026-06-09 23:02:03 -0500

    create project

---
+++ src/main/java/com/project/sistema/chamba_altoque/ChambaAltoqueApplication.java
@@ -0,0 +1,13 @@
+package com.project.sistema.chamba_altoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChambaAltoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChambaAltoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sistema/chamba_altoque/ChambaAltoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sistema.chamba_altoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChambaAltoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
