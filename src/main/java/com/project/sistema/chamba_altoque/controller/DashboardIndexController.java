a40bf482a19f3c67ba7d74bd837d5f7f27ca271e
Diego-Lucar <lucardiego@gmail.com>
2026-06-10 23:34:37 -0500

    add templates dashboard

---
+++ src/main/java/com/project/sistema/chamba_altoque/controller/DashboardIndexController.java
@@ -0,0 +1,30 @@
+package com.project.sistema.chamba_altoque.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class DashboardIndexController {
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboardIndex";
+    }
+    @GetMapping("/usuario")
+    public String usuario() {
+        return "dashboardUsuario";
+    }
+
+    @GetMapping("/freelancer")
+    public String freelancer() {
+        return "dashboardFreelancer";
+    }
+    @GetMapping("/validacion")
+    public String validacion(){
+        return "dashboardValidacion";
+    }
+    
+
+}
