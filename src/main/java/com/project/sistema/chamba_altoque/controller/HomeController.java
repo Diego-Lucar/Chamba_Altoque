195104dd9350e60d79fc4a80a9d04273713f68a4
Maricielo Cabanilla <maricielocv77@gmail.com>
2026-06-22 23:21:55 -0500

    feat: implement search bar in home view

--- src/main/java/com/project/sistema/chamba_altoque/controller/HomeController.java
+++ src/main/java/com/project/sistema/chamba_altoque/controller/HomeController.java
@@ -3,11 +3,21 @@ package com.project.sistema.chamba_altoque.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.project.sistema.chamba_altoque.repository.DistritoRepository;
+import org.springframework.ui.Model;
+
 @Controller
 public class HomeController {
+    
+    private final DistritoRepository distritoRepository;
+
+    public HomeController(DistritoRepository distritoRepository) {
+        this.distritoRepository = distritoRepository;
+    }
 
     @GetMapping("/")
-    public String inicio() {
+    public String inicio(Model model) {
+        model.addAttribute("listarDistritos", distritoRepository.findAll());
         return "inicio";
     }
 
